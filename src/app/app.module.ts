b94788450be0e3f1710c66878dcd78c16c48280b
Quentin RODIC <rodic.quentin.corporation@gmail.com>
2020-01-24 15:10:47 +0100

    Tag component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TagComponent } from './tag/tag.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TagComponent
   ],
   imports: [
     BrowserModule,
