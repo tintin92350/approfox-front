8ccae21c091888724f48ba3d4d5fa5715478dd08
Quentin RODIC <rodic.quentin.corporation@gmail.com>
2020-04-10 16:12:58 +0200

    Toast stay on page when mouse enter component + angular upgrade to version 9

--- src/app/ui/toast/toast.component.ts
+++ src/app/ui/toast/toast.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {ToastMessage} from '../../models/ToastMessage.model';
 import {ToastService} from '../../services/toast.service';
+// @ts-ignore
+import Timeout = NodeJS.Timeout;
 
 @Component({
   selector: 'app-toast',
@@ -11,16 +13,13 @@ export class ToastComponent implements OnInit, OnChanges {
 
   @Input() toastMessage: ToastMessage;
 
+  public timeoutFunction: Timeout;
+
   constructor(private toastService: ToastService) {
   }
 
   ngOnInit() {
-    setTimeout(() => {
-      this.toastMessage.closed = true;
-      setTimeout(() => {
-        this.toastService.update();
-      }, 900);
-    }, this.toastMessage.delay);
+    this.onLeave();
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -47,4 +46,16 @@ export class ToastComponent implements OnInit, OnChanges {
     setTimeout(this.realClose, 300);
   }
 
+  public onHover() {
+    clearTimeout(this.timeoutFunction);
+  }
+
+  public onLeave() {
+    this.timeoutFunction = setTimeout(() => {
+      this.toastMessage.closed = true;
+      setTimeout(() => {
+        this.toastService.update();
+      }, 900);
+    }, this.toastMessage.delay);
+  }
 }
